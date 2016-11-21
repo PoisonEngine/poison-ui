2c2d46986f96c203b8b6367799773119a47563ca
Jacob Jensen <jj_1337@live.dk>
2016-11-21 21:57:52 +0100

    Added font styles to the stylesheet engine

--- src/ui/fonts.d
+++ src/ui/fonts.d
@@ -5,37 +5,67 @@ import std.algorithm : filter, endsWith;
 import std.string : toLower;
 import std.path : stripExtension, baseName;
 
-import dsfml.graphics : Font;
+public import dsfml.graphics : Font;
 
+/// The fonts.
 private Font[string] _fonts;
 
+/// Enumeration of font styles.
 enum FontStyle {
+  /// Normal font style.
   normal = "",
+
+  /// Bold font style.
   bold = "b",
+
+  /// Italic font style.
   italic = "i",
+
+  /// Bold & italic font style.
   boldItalic = "z"
 }
 
+/**
+* Loads all fonts within a specific path.
+* Params:
+*   path =  The path of the fonts.
+*/
 void loadFonts(string path) {
   auto entries = dirEntries(path, SpanMode.depth).filter!(f => f.name.toLower().endsWith(".ttf"));
 
   foreach (string filePath; entries) {
     loadFont(filePath);
   }
-
-  import std.stdio;
-  writeln(_fonts);
 }
 
-void loadFont(string path) {
-  auto font = new Font();
+/**
+* Loads a font by its path or retrieves it from the font cache.
+* Params:
+*   path =  The path of the font. Note: The path can be a name too, but is required with the font-style suffix. Use retrieveFont for easier access.
+*/
+Font loadFont(string path) {
+  auto font = _fonts.get(path, null);
+
+  if (font) {
+    return font;
+  }
+
+  font = new Font();
   font.loadFromFile(path);
 
   auto name = stripExtension(baseName(path));
 
   _fonts[name] = font;
+  _fonts[path] = font;
+  return font;
 }
 
+/**
+* Retrieves a font by a name and style.
+* Params:
+*   fontName =  The name of the font to retrieve.
+*   style =     The style of the font.
+*/
 Font retrieveFont(string fontName, FontStyle style) {
   return _fonts.get(fontName ~ style, null);
 }
